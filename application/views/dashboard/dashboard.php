9a01a608bd2e8c17d3586d96103278db86162c4c
JessenWL <84946132+JessenWL@users.noreply.github.com>
2022-07-18 01:09:25 +0700

    Add files via upload

---
+++ application/views/dashboard/dashboard.php
@@ -0,0 +1,130 @@
+			<!-- begin::Body -->
+			<div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop 	m-container m-container--responsive m-container--xxl m-page__container m-body">
+				<div class="m-grid__item m-grid__item--fluid m-wrapper">
+
+					<!--Begin::Section-->
+					<div class="row">
+						<div class="col-xl-4">
+
+							<!--begin:: Widgets/Announcements 1-->
+							<!--<div class="m-portlet__body">
+								<div class="m-widget19">
+									<div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height: 286px">
+										<img src="<?= base_url('assets/') ?>app/media/img//blog/kamera1.jpg" alt="">
+									</div>
+									<div class="m-widget19__content">
+										<div class="m-widget19__body">
+											<strong>Kamera Daguerreotypes dan Calotypes </strong>Hampir 900 tahun setelah ditemukannya kamera
+											obscura, pada tahun 1937, Joseph Nicephore Niepce yang berkebangsaan Prancis menemukan konsep
+											fotografi yang praktis, yang kemudian dinamakan sebagai Daguerreotypes. Di dalam sebuah kotak kecil
+											dalam lubang cahaya ia menambahkan pelat tembaga dan perak yang ditambahkan dengan uap yodium sehingga kamera generasi ini lebih sensitif terhadap cahaya.
+										</div>
+									</div>
+								</div>
+							</div>-->
+
+							<!--end:: Widgets/Announcements 1-->
+						</div>
+						<div class="col-xl-4">
+
+							<!--begin:: Widgets/Blog-->
+							<--<div class="m-portlet m-portlet--bordered-semi m-portlet--full-height  m-portlet--rounded-force">
+								<div class="m-portlet__head m-portlet__head--fit">
+									<div class="m-portlet__head-caption">-->
+										<!-- <div class="m-portlet__head-action">
+												<button type="button" class="btn btn-sm m-btn--pill  btn-brand">Blog</button>
+											</div> -->
+									<--</div>
+								</div>
+								<div class="m-portlet__body">
+									<div class="m-widget19">
+										<div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height: 286px">
+											<img src="<?= base_url('assets/') ?>app/media/img//blog/kamera4.jpg" alt="">
+										</div>
+										<div class="m-widget19__content">
+											<!-- <div class="m-widget19__header">
+													<div class="m-widget19__user-img">
+														<img class="m-widget19__img" src="<?= base_url('assets/') ?>app/media/img//users/user1.jpg" alt="">
+													</div>
+													<div class="m-widget19__info">
+														<span class="m-widget19__username">
+															Anna Krox
+														</span>
+														<br>
+														<span class="m-widget19__time">
+															UX/UI Designer, Google
+														</span>
+													</div>
+													<div class="m-widget19__stats">
+														<span class="m-widget19__number m--font-brand">
+															18
+														</span>
+														<span class="m-widget19__comment">
+															Comments
+														</span>
+													</div>
+												</div> -->
+											<--<div class="m-widget19__body">
+												<strong>Kamera Dry Plates </strong>Kamera plat kering mulai digunakan orang semenjak tahun 1857. Kamera yang satu ini merupakan buah
+												karya dari Desire van Monckoven. Empat belas tahun kemudian, kamera plat kering ini dimodifikasi
+												oleh Richard Leach Maddox yang berhasil menciptakan kamera plat basah yang memiliki kualitas dan
+												kecepatan yang lebih baik dalam hal pengambilan gambar.
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>-->
+
+							<!--end:: Widgets/Blog-->
+						</div>
+						<div class="col-xl-4">
+
+							<!--begin:: Widgets/Sales States-->
+							<div class="m-portlet m-portlet--full-height "><br><br><br>
+								<div class="m-portlet__body">
+									<div class="m-widget19">
+										<div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height: 286px">
+											<img src="<?= base_url('assets/') ?>app/media/img//blog/kamera3.jpg" alt="">
+										</div>
+										<div class="m-widget19__content">
+											<div class="m-widget19__body">
+												<strong>Mirrorless Camera</strong> Inovasi terbaru dari pengembangan teknologi kamera terbaru
+											kamera Mirrorless merupakan titik teratas dari pengembangan selama ini dengan bentuk yang
+											lebih kecil dan praktis daripada kamera DSLR.
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!--Begin::Section-->
+					<div class="row">
+						<div class="col-xl-4">
+
+							<!--begin:: Widgets/Announcements 1-->
+							<!--<div class="m-portlet__body">
+								<div class="m-widget19">
+									<div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height: 286px">
+										<img src="<?= base_url('assets/') ?>app/media/img//blog/kamera4.jpg" alt="">
+									</div>
+									<div class="m-widget19__content">
+										<div class="m-widget19__body">
+											<strong>Mirrorless Camera</strong> Inovasi terbaru dari pengembangan teknologi kamera terbaru
+											kamera Mirrorless merupakan titik teratas dari pengembangan selama ini dengan bentuk yang
+											lebih kecil dan praktis daripada kamera DSLR
+										</div>
+									</div>
+								</div>
+							</div>-->
+
+							<!--end:: Widgets/Announcements 1-->
+						</div>
+
+						<!--End::Section-->
+					</div>
+				</div>
+			</div>
+
+			<!-- end::Body -->
